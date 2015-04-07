da2abf9ea1318989849afe86466febafad679deb
wanirepo <clinpsywoo@gmail.com>
2015-04-06 21:11:27 -0600

    add data and code

---
+++ predict_example.m
@@ -0,0 +1,9 @@
+%% example of reading images using fmri_data
+
+% basedir = '/Volumes/engram/labdata/current/BMRK3';
+% imgs = filenames(fullfile(basedir, 'Imaging/*/Models/Temp/beta_000[1-5].img'));
+% mask = fullfile(basedir,'pain_2s_z_val_FDR05_pos.nii');
+% dat = fmri_data(imgs, mask);
+
+%% Load data
+laod data;
